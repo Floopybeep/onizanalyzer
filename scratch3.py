8b585514376c22d82cb70a811896b169935be1ae
Floopybeep <71326597+Floopybeep@users.noreply.github.com>
2022-12-21 00:06:02 +0900

    updated stuff

--- scratch3.py
+++ scratch3.py
@@ -1,7 +1,7 @@
 import sc2reader
 
 # path = "C:/Users/USER/PycharmProjects/onizanalyzer/replays/Oh No It's Zombies Arctic Map (245).SC2Replay"
-path = "C:/Users/wooil/Documents/StarCraft II/Accounts/12861615/1-S2-1-5777751/Replays/Multiplayer/Oh No It's Zombies Arctic Map (9).SC2Replay"
+path = "C:/Users/wooil/Documents/StarCraft II/Accounts/12861615/1-S2-1-5777751/Replays/Multiplayer/Oh No It's Zombies Arctic Map (10).SC2Replay"
 
 replay = sc2reader.load_replay(path, load_level=3)
 
@@ -34,29 +34,31 @@ for event in replay.events:
         if event.name == 'PlayerStatsEvent' and event.pid < 10 and event.player.play_race == 'Zerg':
             zergstat = event
 
-        if event.name == 'UpgradeCompleteEvent':
-            if event.upgrade_type_name not in {'LongRangeScope', 'FragGrenadeUnlocked', 'ExplorationDroidUnlocked',
-                                               'TESTEquippedExplorationDroidUnlocked', 'MiningChargeUnlocked',
-                                               'MotionSensorUnlocked', }:
-                # if event.pid == 2:
-                eventlist.append(event)
+        # if event.name == 'UpgradeCompleteEvent':
+        #     if event.upgrade_type_name not in {'LongRangeScope', 'FragGrenadeUnlocked', 'ExplorationDroidUnlocked',
+        #                                        'TESTEquippedExplorationDroidUnlocked', 'MiningChargeUnlocked',
+        #                                        'MotionSensorUnlocked', }:
+        #         # if event.pid == 2:
+        #         eventlist.append(event)
 
         # elif event.name == 'UnitBornEvent':
         #     if event.unit_type_name not in {'DigesterCreepSprayTargetUnit', 'DigesterCreepSprayUnit',
         #                                     'InfestedCivilian', 'InfestedExploder', 'Baneling', 'Kaboomer',
         #                                     'InfestedExploderBurrowed', 'BanelingBurrowed', 'LocustMP3',
         #                                     'InfestorTerran', 'DehakaMirrorImage', 'NovaAcidPuddle2',
-        #                                     'PrimalTownHallUprooted', 'Kaboomer2', 'InfestorTerranBurrowed',
+        #                                     'Kaboomer2', 'InfestorTerranBurrowed',
         #                                     'AcidPuddle', 'HotSRaptor', 'RoachCorpser',
-        #                                     'ZergDropPod', 'CreepPod', 'DevastatorLeaping'}:
+        #                                     'ZergDropPod', 'CreepPod', 'DevastatorLeaping', 'FlyingZombie2',
+        #                                     'Mutalisk', 'Devourer2', 'KerriganInfestBroodling'}:
         #         if event.upkeep_pid != 10:
         #             eventlist.append(event)
-        # elif event.name == 'UnitTypeChangeEvent':
-        #     if event.unit_type_name not in {'SpiderMine', 'SpiderMineBurrowed', 'InfestedCivilian', 'SentryGun',
-        #                                     'SpecOpsSentryGun', 'SentryGunUnderground', 'SpecOpsSentryGun2',
-        #                                     'InfestedTerranCampaignBurrowed', 'LabTurretUp', 'AutoTurret2',
-        #                                     'InfestedExploder', 'Baneling', 'Kaboomer', 'ToxicNestBurrowed'}:
-        #         eventlist.append(event)
+
+        elif event.name == 'UnitTypeChangeEvent':
+            if event.unit_type_name not in {'SpiderMine', 'SpiderMineBurrowed', 'InfestedCivilian', 'SentryGun',
+                                            'SpecOpsSentryGun', 'SentryGunUnderground', 'SpecOpsSentryGun2',
+                                            'InfestedTerranCampaignBurrowed', 'LabTurretUp', 'AutoTurret2',
+                                            'InfestedExploder', 'Baneling', 'Kaboomer', 'ToxicNestBurrowed'}:
+                eventlist.append(event)
 
 print(eventlist)
 
