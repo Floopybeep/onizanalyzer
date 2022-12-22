28fbea18a1ec8861c4463fc71465b064db4c27cf
Floopybeep <71326597+Floopybeep@users.noreply.github.com>
2022-12-22 23:12:38 +0900

    updated stuff

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ from functions import *
 from guifunctions import NewClass
 
 
-mainclass = mainfunctionclass()
-NC = NewClass(mainclass)
+# mainclass = mainfunctionclass()
+# NC = NewClass(mainclass)
 
 # Right now, GUI takes mainclass and makes mainclass_copy, but it does not execute for some reason
+
+
+if __name__ == '__main__':
+    mainclass = mainfunctionclass()
+    replaypath = "C:/Users/wooil/Documents/StarCraft II/Accounts/12861615/1-S2-1-5777751/Replays/Multiplayer/Oh No It's Zombies"
+    textfilepath = "C:/Users/wooil/Documents/StarCraft II/Accounts/12861615/1-S2-1-5777751/Replays/Multiplayer/Oh No It's Zombies/textfile"
+
+    mainclass.replayfolderpath = replaypath
+    mainclass.textfolderpath = textfilepath
+    mainclass.get_replays_from_folder()
+    mainclass.send_replays_to_self()
+
+
